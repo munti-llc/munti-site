618379c7989c83f3c17ecbe7cd3d96540f0e120b
lockjio <jio.buenviaje@gmail.com>
2020-06-10 02:26:53 -0400

    Initialize SCSS transition

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./assets/animate.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/main.css";
-import "./assets/responsive.css";
+import "./assets/main.scss";
+import "./assets/responsive.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "popper.js";
 
